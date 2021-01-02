8f4ea21a8d6aec864a2c12a178fe4e6a9baaadfb
DevCodeOne <christian.r.dev@googlemail.com>
2021-01-02 16:15:56 +0100

    Added missing files

---
+++ main/webserver.h
@@ -0,0 +1,122 @@
+#pragma once
+
+#include <cstdio>
+#include <string_view>
+
+#include "esp_http_server.h"
+#include "esp_log.h"
+
+template <typename T = void>
+class webserver final {
+   public:
+    webserver() {
+        httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+        config.uri_match_fn = httpd_uri_match_wildcard;
+
+        httpd_start(&m_http_server_handle, &config);
+
+        httpd_uri_t file_handler = {.uri = "/*",
+                                    .method = HTTP_GET,
+                                    .handler = webserver::get_file,
+                                    .user_ctx = this};
+
+        register_handler(file_handler);
+    }
+
+    webserver(const webserver &other) = delete;
+    webserver(webserver &&other) = delete;
+
+    ~webserver() {}
+
+    webserver &operator=(const webserver &other) = delete;
+    webserver &operator=(webserver &&other) = delete;
+
+    void register_handler(httpd_uri_t handler) {
+        httpd_register_uri_handler(m_http_server_handle, &handler);
+    }
+
+   private:
+    static esp_err_t get_file(httpd_req_t *req);
+
+    httpd_handle_t m_http_server_handle = nullptr;
+};
+
+inline esp_err_t set_content_type_from_file(httpd_req_t *req,
+                                            const char *filepath) {
+    auto ends_with = [](std::string_view current_view, std::string_view ending) {
+        const auto view_len = current_view.length();
+        const auto ending_len = current_view.length();
+        if (view_len < ending_len) {
+            return false;
+        }
+
+        return current_view.substr(view_len - ending_len) == ending;
+    };
+
+    const char *type = "text/plain";
+    if (ends_with(filepath, ".html")) {
+        type = "text/html";
+    } else if (ends_with(filepath, ".js")) {
+        type = "application/javascript";
+    } else if (ends_with(filepath, ".css")) {
+        type = "text/css";
+    } else if (ends_with(filepath, ".png")) {
+        type = "image/png";
+    } else if (ends_with(filepath, ".ico")) {
+        type = "image/x-icon";
+    } else if (ends_with(filepath, ".svg")) {
+        type = "text/xml";
+    }
+    return httpd_resp_set_type(req, type);
+}
+
+template <typename T>
+esp_err_t webserver<T>::get_file(httpd_req_t *req) {
+    char filepath[256]{"/storage"};
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+
+    if (req->uri[strlen(req->uri) - 1] == '/') {
+        strncat(filepath, "/index.html", sizeof(filepath) - 1);
+    } else {
+        strncat(filepath, req->uri, sizeof(filepath) - 1);
+    }
+
+    int fd = open(filepath, O_RDONLY, 0);
+    if (fd == -1) {
+        ESP_LOGE(__PRETTY_FUNCTION__, "Failed to open file : %s", filepath);
+        /* Respond with 500 Internal Server Error */
+        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR,
+                            "Failed to read existing file");
+        return ESP_FAIL;
+    }
+
+    set_content_type_from_file(req, filepath);
+
+    char chunk[512];
+    ssize_t read_bytes;
+    do {
+        /* Read file in chunks into the scratch buffer */
+        read_bytes = read(fd, chunk, sizeof(chunk));
+        if (read_bytes == -1) {
+            ESP_LOGE(__PRETTY_FUNCTION__, "Failed to read file : %s", filepath);
+        } else if (read_bytes > 0) {
+            /* Send the buffer contents as HTTP response chunk */
+            if (httpd_resp_send_chunk(req, chunk, read_bytes) != ESP_OK) {
+                close(fd);
+                ESP_LOGE(__PRETTY_FUNCTION__, "File sending failed!");
+                /* Abort sending file */
+                httpd_resp_sendstr_chunk(req, NULL);
+                /* Respond with 500 Internal Server Error */
+                httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR,
+                                    "Failed to send file");
+                return ESP_FAIL;
+            }
+        }
+    } while (read_bytes > 0);
+    /* Close file after sending complete */
+    close(fd);
+    ESP_LOGI(__PRETTY_FUNCTION__, "File sending complete");
+    /* Respond with an empty chunk to signal HTTP response completion */
+    httpd_resp_send_chunk(req, nullptr, 0);
+    return ESP_OK;
+}
---
+++ main/wifi_manager.h
@@ -0,0 +1,121 @@
+#pragma once
+
+#include <array>
+#include <chrono>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <limits>
+
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_wifi.h"
+#include "freertos/event_groups.h"
+#include "nvs_flash_utils.h"
+
+static constexpr uint8_t wifi_connected_bit = BIT0;
+static constexpr uint8_t wifi_fail_bit = BIT1;
+
+template <wifi_mode_t Mode>
+class wifi_manager;
+
+struct wifi_credentials {
+    std::array<char, 32> ssid;
+    std::array<char, 32> password;
+};
+
+enum struct wifi_reconnect_policy : uint32_t {
+    infinite = std::numeric_limits<uint32_t>::max(),
+    never = 0,
+    once = 1
+};
+
+template <wifi_mode_t Mode>
+struct wifi_config;
+
+template <>
+struct wifi_config<wifi_mode_t::WIFI_MODE_STA> {
+    wifi_credentials creds;
+    wifi_reconnect_policy reconnect_tries{wifi_reconnect_policy::infinite};
+    std::chrono::milliseconds retry_time{1000};
+};
+
+template <>
+class wifi_manager<wifi_mode_t::WIFI_MODE_STA> {
+   public:
+    wifi_manager(const wifi_config<wifi_mode_t::WIFI_MODE_STA> &config)
+        : m_config(config), m_wifi_event_group(xEventGroupCreate()) {
+        tcpip_adapter_init();
+
+        wifi_init_config_t tmp_init_conf = WIFI_INIT_CONFIG_DEFAULT();
+        esp_wifi_init(&tmp_init_conf);
+
+        esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
+                                   &wifi_manager::event_handler, this);
+        esp_event_handler_register(IP_EVENT, ESP_EVENT_ANY_ID,
+                                   &wifi_manager::event_handler, this);
+
+        std::memcpy(m_wifi_conf.sta.ssid, m_config.creds.ssid.data(),
+                    strlen(m_config.creds.ssid.data()));
+        std::memcpy(m_wifi_conf.sta.password, m_config.creds.password.data(),
+                    strlen(m_config.creds.password.data()));
+
+        m_wifi_conf.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+
+        esp_wifi_set_mode(WIFI_MODE_STA);
+        esp_wifi_set_config(ESP_IF_WIFI_STA, &m_wifi_conf);
+        esp_wifi_start();
+    }
+
+    ~wifi_manager() {
+        esp_event_handler_unregister(WIFI_EVENT, IP_EVENT_STA_GOT_IP,
+                                     &wifi_manager::event_handler);
+        esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP,
+                                     &wifi_manager::event_handler);
+        vEventGroupDelete(m_wifi_event_group);
+    }
+
+    // TODO: add max wait time
+    bool await_connection() {
+        EventBits_t bits = xEventGroupWaitBits(
+            m_wifi_event_group, wifi_connected_bit | wifi_fail_bit, pdFALSE,
+            pdFALSE, portMAX_DELAY);
+
+        return (bits & wifi_connected_bit) && !(bits & wifi_fail_bit);
+    }
+
+    operator bool() const;
+
+    // TODO: add delay to reconnect
+    static void event_handler(void *arg, esp_event_base_t event_base,
+                              int32_t event_id, void *event_data) {
+        auto thiz = reinterpret_cast<wifi_manager *>(arg);
+        if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+            esp_wifi_connect();
+        } else if (event_base == WIFI_EVENT &&
+                   event_id == WIFI_EVENT_STA_DISCONNECTED) {
+            if (thiz->retry_num <
+                static_cast<uint32_t>(thiz->m_config.reconnect_tries)) {
+                esp_wifi_connect();
+                thiz->retry_num++;
+                ESP_LOGI(__PRETTY_FUNCTION__, "retry to connect to the AP");
+            } else {
+                xEventGroupSetBits(thiz->m_wifi_event_group, wifi_fail_bit);
+            }
+            ESP_LOGI(__PRETTY_FUNCTION__, "connect to the AP fail");
+        } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+            ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+            ESP_LOGI(__PRETTY_FUNCTION__, "got ip:" IPSTR,
+                     IP2STR(&event->ip_info.ip));
+            thiz->retry_num = 0;
+            xEventGroupSetBits(thiz->m_wifi_event_group, wifi_connected_bit);
+        }
+    }
+
+   private:
+    wifi_config<wifi_mode_t::WIFI_MODE_STA> m_config;
+    wifi_config_t m_wifi_conf;
+    EventGroupHandle_t m_wifi_event_group;
+    nvs_flash m_nvs;
+    int32_t retry_num = 0;
+};
