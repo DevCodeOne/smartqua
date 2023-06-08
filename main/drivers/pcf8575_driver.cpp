834f429a74b64f6a2a099f1bff463acd35792a02
DevCodeOne <christian.r.dev@googlemail.com>
2023-06-09 00:02:24 +0200

    Added pcf8575_driver and switch_driver files

---
+++ main/drivers/pcf8575_driver.cpp
@@ -0,0 +1,278 @@
+#include "pcf8575_driver.h"
+#include "drivers/device_types.h"
+#include "utils/logger.h"
+
+#include <algorithm>
+#include <chrono>
+#include <thread>
+#include <utility>
+#include <string_view>
+#include <optional>
+#include <bit>
+
+#include <pcf8575.h>
+
+std::optional<Pcf8575Driver> Pcf8575Driver::create_driver(const DeviceConfig*config) {
+    auto driver_data = reinterpret_cast<const Pcf8575DriverData *>(config->device_config.data());
+    auto sdaPin = device_resource::get_gpio_resource(driver_data->sdaPin, gpio_purpose::bus);
+    auto sclPin = device_resource::get_gpio_resource(driver_data->sclPin, gpio_purpose::bus);
+
+    if (!sdaPin || !sclPin) {
+        Logger::log(LogLevel::Warning, "I2C Port cannot be used, since one or more pins are already reserved for a different purpose");
+        return std::nullopt;
+    }
+
+    if (!add_address(driver_data->addr)) {
+        Logger::log(LogLevel::Warning, "The device is already in use");
+        return std::nullopt;
+    }
+
+    i2c_dev_t device{};
+    // Has to be set specificaly
+    device.cfg.clk_flags = 0;
+    auto result = pcf8575_init_desc(&device, static_cast<uint8_t>(driver_data->addr), 0, 
+                    static_cast<gpio_num_t>(driver_data->sdaPin), 
+                    static_cast<gpio_num_t>(driver_data->sclPin));
+
+    if (result != ESP_OK) {
+        Logger::log(LogLevel::Warning, "Couldn't find any ads111x devices on port %d with address %d", 
+            0, static_cast<int>(driver_data->addr));
+        remove_address(driver_data->addr);
+    }
+    
+    return Pcf8575Driver(config, std::move(device));
+}
+
+// TODO: maybe make pins configureable
+std::optional<Pcf8575Driver> Pcf8575Driver::create_driver(const std::string_view input, DeviceConfig&device_conf_out) {
+    Pcf8575Address address = Pcf8575Address::Invalid;
+    unsigned int scl = static_cast<uint8_t>(sclDefaultPin);
+    unsigned int sda = static_cast<uint8_t>(sdaDefaultPin);
+
+    Logger::log(LogLevel::Info, "%.*s", input.size(), input.data());
+    // TODO: write json_scanf_single
+    json_scanf(input.data(), input.size(), R"({ address : %M, sclPin : %u, sdaPin : %u })", 
+       json_scanf_single<decltype(address)>, &address, 
+        &scl, &sda);
+
+    if (address == Pcf8575Address::Invalid) {
+        Logger::log(LogLevel::Warning, "Invalid address for pcf8575_driver : %d", (int) address);
+        return std::nullopt;
+    }
+
+    Logger::log(LogLevel::Info, "Address for pcf8575_driver : %x", (int) address);
+
+    auto sclPin = device_resource::get_gpio_resource(static_cast<gpio_num_t>(scl), gpio_purpose::bus);
+    auto sdaPin = device_resource::get_gpio_resource(static_cast<gpio_num_t>(sda), gpio_purpose::bus);
+
+    if (!sclPin || !sdaPin) {
+        Logger::log(LogLevel::Warning, "GPIO pins couldn't be reserved");
+        return std::nullopt;
+    }
+
+    if (!add_address(address)) {
+        Logger::log(LogLevel::Warning, "Duplicate address, or address list is full");
+        return std::nullopt;
+    }
+
+    i2c_dev_t device{};
+    // Has to be set specificaly
+    device.cfg.clk_flags = 0;
+    auto result = pcf8575_init_desc(&device, static_cast<uint8_t>(address), 0, static_cast<gpio_num_t>(sda), static_cast<gpio_num_t>(scl));
+
+    if (result != ESP_OK) {
+        Logger::log(LogLevel::Warning, "Couldn't find any pcf8575 devices on port %d with address %d", 
+            0, static_cast<int>(address));
+        remove_address(address);
+        return std::nullopt;
+    }
+    
+    Logger::log(LogLevel::Info, "Initialized pcf8575 @ %x", (int) address);
+
+    Pcf8575DriverData data { 
+        .addr = address, 
+        .sdaPin = static_cast<gpio_num_t>(sda),
+        .sclPin = static_cast<gpio_num_t>(scl)
+    };
+    std::memcpy(reinterpret_cast<void *>(device_conf_out.device_config.data()), &data, sizeof(Pcf8575DriverData));
+    device_conf_out.device_driver_name =  Pcf8575Driver::name;
+
+    return Pcf8575Driver(&device_conf_out, std::move(device));
+}
+
+Pcf8575Driver::Pcf8575Driver(const DeviceConfig*conf, i2c_dev_t device) 
+    : m_conf(conf), m_device(std::move(device)) { 
+}
+
+Pcf8575Driver::Pcf8575Driver(Pcf8575Driver &&other) : m_conf(other.m_conf), m_device(other.m_device) {
+    other.mReadingThread.request_stop();
+    if (other.mReadingThread.joinable()) {
+        other.mReadingThread.join();
+    }
+
+    other.m_conf = nullptr;
+    std::memset(reinterpret_cast<void *>(&other.m_device), 0, sizeof(i2c_dev_t));
+
+    mReadingThread = std::jthread(&Pcf8575Driver::updatePinsThread, this);
+ }
+
+ Pcf8575Driver &Pcf8575Driver::operator=(Pcf8575Driver &&other) {
+    using std::swap;
+
+    other.mReadingThread.request_stop();
+    if (other.mReadingThread.joinable()) {
+        other.mReadingThread.join();
+    }
+
+    swap(m_conf, other.m_conf);
+    swap(m_device, other.m_device);
+
+    mReadingThread = std::jthread(&Pcf8575Driver::updatePinsThread, this);
+
+    return *this;
+}
+
+Pcf8575Driver::~Pcf8575Driver() { 
+    if (!m_conf) {
+        return;
+    }
+
+    mReadingThread.request_stop();
+    if (mReadingThread.joinable()) {
+        mReadingThread.join();
+    }
+    remove_address(reinterpret_cast<Pcf8575DriverData *>(m_conf->device_config.data())->addr);
+}
+
+// TODO: fix that values can be written here
+DeviceOperationResult Pcf8575Driver::write_value(std::string_view what, const device_values &value) { 
+    const auto *config  = reinterpret_cast<const Pcf8575DriverData *>(&m_conf->device_config);
+
+    uint8_t toSet = 0;
+    uint8_t pin = 0xff;
+    auto convResult = std::from_chars(what.data(), what.data() + what.size(), pin);
+
+    if (convResult.ec != std::errc() || pin >= 16) {
+        return DeviceOperationResult::failure;
+    }
+
+    if (value.enable().has_value()) {
+        toSet = std::min<uint8_t>(*value.enable(), 1);
+    } else if (value.percentage().has_value()) {
+        toSet = std::min<uint8_t>(*value.percentage(), 1);
+    } else {
+        return DeviceOperationResult::failure;
+    }
+
+    // Only write values in here which are actually written. Otherwise reading a zero with port_read and then writing it, 
+    // would convert an input to an output
+    writtenValue &= std::numeric_limits<uint16_t>::max() & (toSet << pin);
+    auto result = pcf8575_port_write(&m_device, config->pinValues);
+
+    if (result != ESP_OK) {
+        return DeviceOperationResult::failure;
+    }
+
+    return DeviceOperationResult::ok;
+}
+
+DeviceOperationResult Pcf8575Driver::read_value(std::string_view what, device_values &value) const {
+    const auto *config  = reinterpret_cast<const Pcf8575DriverData *>(&m_conf->device_config);
+    Logger::log(LogLevel::Info, "Reading address : %u value to read : %.*s", 
+        static_cast<uint32_t>(config->addr), what.size(), what.data());
+
+    uint8_t pin = 0xff;
+
+    auto convResult = std::from_chars(what.data(), what.data() + what.size(), pin);
+
+    if (convResult.ec != std::errc() || pin >= 16) {
+        return DeviceOperationResult::failure;
+    }
+
+    value.enable(readValue & pin);
+
+    return DeviceOperationResult::ok;
+}
+
+// TODO: implement both
+DeviceOperationResult Pcf8575Driver::get_info(char *output_buffer, size_t output_buffer_len) const {
+    json_out out = JSON_OUT_BUF(output_buffer, output_buffer_len);
+    json_printf(&out, "{}");
+    return DeviceOperationResult::ok;
+}
+
+DeviceOperationResult Pcf8575Driver::call_device_action(DeviceConfig*conf, const std::string_view &action, const std::string_view &json) {
+    return DeviceOperationResult::ok;
+}
+
+DeviceOperationResult Pcf8575Driver::update_runtime_data() {
+    return DeviceOperationResult::not_supported;
+}
+
+void Pcf8575Driver::updatePinsThread(std::stop_token token, Pcf8575Driver *instance) {
+    using namespace std::chrono_literals;
+
+    const auto *config  = reinterpret_cast<const Pcf8575DriverData *>(&instance->m_conf->device_config);
+    while(!token.stop_requested()) {
+        auto beforeReading = std::chrono::steady_clock::now();
+        
+        uint16_t readValue = 0;
+        auto result = pcf8575_port_read(&instance->m_device, &readValue);
+        if (result == ESP_OK) {
+            Logger::log(LogLevel::Info, "Read value : %u for pcf8575", static_cast<uint32_t>(readValue));
+        } else {
+            continue;
+        }
+
+        instance->readValue.exchange(readValue);
+
+        const auto duration = std::chrono::steady_clock::now() - beforeReading;
+        std::this_thread::sleep_for(duration < 5s ? 5s - duration : 500ms);
+    }
+    Logger::log(LogLevel::Info, "Exiting updatePinsThread");
+}
+
+bool Pcf8575Driver::add_address(Pcf8575Address address) {
+    std::unique_lock instance_guard{_instance_mutex};
+
+    bool adress_already_exists = std::any_of(Pcf8575Driver::_device_addresses.cbegin(), Pcf8575Driver::_device_addresses.cend(), 
+        [&address](const auto &already_found_address) {
+            return already_found_address.has_value() && *already_found_address == address;
+        });
+
+    if (adress_already_exists) {
+        Logger::log(LogLevel::Warning, "No new address, don't add this address");
+        return false;
+    }
+
+    auto first_empty_slot = std::find(Pcf8575Driver::_device_addresses.begin(), Pcf8575Driver::_device_addresses.end(), std::nullopt);
+
+    // No free addresses
+    if (first_empty_slot == Pcf8575Driver::_device_addresses.cend()) {
+        return false;
+    }
+
+    Logger::log(LogLevel::Info, "Adding address : %x ", (int) address);
+    *first_empty_slot = address;
+
+    return true;
+}
+
+bool Pcf8575Driver::remove_address(Pcf8575Address address) {
+    std::unique_lock instance_guard{_instance_mutex};
+
+    auto found_address = std::find_if(Pcf8575Driver::_device_addresses.begin(), Pcf8575Driver::_device_addresses.end(), 
+        [&address](auto &current_address) {
+            return current_address.has_value() && *current_address == address;
+        });
+
+    if (found_address == Pcf8575Driver::_device_addresses.end()) {
+        Logger::log(LogLevel::Info, "Couldn't remove address  : %x : not found ", (int) address);
+        return false;
+    }
+
+    Logger::log(LogLevel::Info, "Removing address : %x ", (int) address);
+    *found_address = std::nullopt;
+
+    return true;
+}
---
+++ main/drivers/pcf8575_driver.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "pcf8575.h"
+
+#include <atomic>
+#include <cstdint>
+#include <charconv>
+#include <thread>
+#include <limits>
+#include <string_view>
+#include <type_traits>
+
+#include "drivers/device_types.h"
+#include "drivers/device_resource.h"
+#include "build_config.h"
+
+enum struct Pcf8575Address : uint8_t { Invalid = 0xFF };
+
+template<>
+struct read_from_json<Pcf8575Address> {
+    static void read(const char *str, int len, Pcf8575Address &addr) {
+        std::string_view input(str, len);
+        int base = 10;
+
+        addr = Pcf8575Address::Invalid;
+        const char *start = str;
+
+        if (input.starts_with("0b")) {
+            str += 2;
+            base = 2;
+        } else if (input.starts_with("0x")) {
+            str += 2;
+            base = 16;
+        } else if (input.starts_with("0")) {
+            str += 2;
+            base = 8;
+        } 
+
+        const char *end = str + len;
+        auto asIntValue{static_cast<std::underlying_type_t<Pcf8575Address>>(Pcf8575Address::Invalid)};
+        auto result = std::from_chars(start, end, asIntValue, base);
+
+        if (result.ec == std::errc()) {
+            addr = static_cast<Pcf8575Address>(asIntValue);
+        }
+    }
+};
+
+struct Pcf8575DriverData final {
+    Pcf8575Address addr;
+    uint16_t pinValues = std::numeric_limits<uint16_t>::max();
+    gpio_num_t sdaPin = static_cast<gpio_num_t>(sdaDefaultPin);
+    gpio_num_t sclPin = static_cast<gpio_num_t>(sclDefaultPin);
+};
+
+class Pcf8575Driver final {
+    public:
+        static inline constexpr char name[] = "pcf8575_driver";
+
+        Pcf8575Driver(const Pcf8575Driver &other) = delete;
+        Pcf8575Driver(Pcf8575Driver &&other);
+        ~Pcf8575Driver();
+
+        Pcf8575Driver &operator=(const Pcf8575Driver &other) = delete;
+        Pcf8575Driver &operator=(Pcf8575Driver &&other);
+
+        static std::optional<Pcf8575Driver> create_driver(const std::string_view input, DeviceConfig&device_conf_out);
+        static std::optional<Pcf8575Driver> create_driver(const DeviceConfig*config);
+
+        DeviceOperationResult write_value(std::string_view what, const device_values &value);
+        DeviceOperationResult read_value(std::string_view what, device_values &value) const;
+        DeviceOperationResult get_info(char *output, size_t output_buffer_len) const;
+        DeviceOperationResult call_device_action(DeviceConfig*conf, const std::string_view &action, const std::string_view &json);
+        DeviceOperationResult update_runtime_data();
+    private:
+        Pcf8575Driver(const DeviceConfig*conf, i2c_dev_t device);
+        static void updatePinsThread(std::stop_token token, Pcf8575Driver *instance);
+
+        static bool add_address(Pcf8575Address address);
+        static bool remove_address(Pcf8575Address address);
+
+        const DeviceConfig*m_conf;
+
+        mutable i2c_dev_t m_device;
+        std::atomic_uint16_t readValue;
+        uint16_t writtenValue = std::numeric_limits<uint16_t>::max();
+        std::jthread mReadingThread;
+
+        static inline std::array<std::optional<Pcf8575Address>, 4> _device_addresses;
+        static inline std::shared_mutex _instance_mutex;
+};
